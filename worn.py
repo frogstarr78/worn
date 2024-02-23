df7fa21e05e2c2ed031764d2f13bd82648fd8739
Scott Noel-Hemming <frogstarr78@gmail.com>
2024-02-23 00:33:12 -0800

    Simplifying. Adding more output functionality

--- worn.py
+++ worn.py
@@ -7,6 +7,7 @@ from uuid import uuid4, UUID
 import redis
 import argparse
 import datetime
+import functools
 from urllib.parse import urlparse
 from typing import *
 import re
@@ -48,7 +49,7 @@ def gui() -> None:
   frm = ttk.LabelFrame(root, text='Project', underline=0, padding=11)
   frm.grid()
 
-  _projects = sorted(db('hvals', 'projects'), key=str.casefold)
+  _projects = [project for (pid, project) in all_projects().items()]
   c = ttk.Combobox(frm, values=_projects, textvariable=proj, width=len(max(_projects, key=len))-10)
   last_id, last_name, last_state, last_when = last_project()
   debug(last_name)
@@ -92,10 +93,11 @@ def log(id, project, state, at=now()):
   _ts = str(at.timestamp()).replace('.', '')[:13]
   db('hsetnx', 'projects', str(id), project)
   db('hsetnx', 'projects', project.casefold(), str(id))
-  db('xadd', 'logs', dict(project=str(id), action=state), id=f'{_ts}-*')
+  db('xadd', 'logs', dict(project=str(id), action=state), id=f'{_ts:0<13}-*')
   db('hset', 'last', mapping=dict(action=state, project=str(id), when=_ts))
   db('save')
 
+@functools.cache
 def load_project(project:Union[None, str, UUID]) -> Tuple[UUID, str]:
   if project is None:
     debug(f"Project {project!r} was empty.")
@@ -124,7 +126,8 @@ def load_project(project:Union[None, str, UUID]) -> Tuple[UUID, str]:
     debug(f'Unable to find or create a new project {project} of type {type(project)}.')
     raise Exception(f'Unable to find or create a new project {project} of type {type(project)}.')
 
-def last_project() -> tuple:
+@functools.cache
+def last_project() -> tuple[UUID, str, str, datetime.datetime]:
   if not db('exists', 'last'):
     raise Exception('No previous projects logged')
 
@@ -162,6 +165,12 @@ def stop_project(project:Union[str, UUID, None], at:datetime=now()) -> None:
   else:
     log(current_project_id, current_project_name, 'stopped', at)
 
+def rename(old:Union[str, UUID], new:str) -> None:
+  id, old_project = load_project(old)
+  db('hset', 'projects', str(id), new)
+  db('hset', 'projects', new.casefold(), str(id))
+  db('save')
+
 def remove(project:Union[UUID, str]) -> None:
   project_id, project_name = load_project(project)
   for timeid, log in db('xrange', 'logs', '-', '+'):
@@ -172,32 +181,14 @@ def remove(project:Union[UUID, str]) -> None:
   db('hdel', 'projects', str(project_id))
   db('save')
 
-def show_projects() -> None:
-  for project_name in sorted([key.casefold() for key in db('hvals', 'projects') if not isuuid(key)]):
-    pid = db('hget', 'projects', project_name)
-    project = db('hget', 'projects', pid)
-    print(f'{pid}: {project}')
+@functools.cache
+def all_projects() -> dict[UUID, str]:
+  return dict([(UUID(pid), project) for pid, project in sorted(db('hgetall', 'projects').items(), key=lambda kv: kv[1].casefold()) if isuuid(pid)])
 
 def stream_time(stream_id:str) -> str:
   unix_time_mills, seq = stream_id.split('-')
   timestamp_mills = float('.'.join([unix_time_mills[:10], unix_time_mills[10:]]))
   return fromtimestamp(timestamp_mills).isoformat(' ', timespec='seconds')
-
-def show_logs(project:Union[None, UUID]=None) -> None:
-  for tid, log in db('xrange', 'logs', '-', '+'):
-    log_id, log_project = load_project(log.get('project'))
-    if project is None or log_id == project:
-      print(f"{stream_time(tid)} action {log.get('action')!r} project {log_project!r}")
-
-def show_id(id:UUID) -> None:
-  print(db('hget', 'projects', str(id)))
-
-def last() -> None:
-  if not db('exists', 'last'):
-    debug('There is not current last project.')
-  else:
-    last_id, last_name, last_state, last_when = last_project()
-    print(f"Last project: {last_name!r}; action: {last_state!r}; at: {last_when}.")
 
 def weeks_long(ts:int) -> int:
   return int(ts/(3600*24*7))
@@ -214,7 +205,7 @@ def minutes_long(ts:int) -> int:
 def seconds_long(ts:int) -> int:
   return int(ts%60)
 
-def csv_format(stats:dict, at:Union[datetime.datetime, None]=None, largest_scale:str='h', still_running:Union[UUID, None]=None) -> None:
+def csv_format(stats:dict[str, str], at:Union[datetime.datetime, None]=None, largest_scale:str='h', still_running:Union[UUID, None]=None, include_all:bool=False) -> None:
   print('Time spent report')
   if largest_scale == 'w':   print('weeks,days,hours,minutes,seconds', end=',')
   elif largest_scale == 'd': print('days,hours,minutes,seconds', end=',')
@@ -222,8 +213,16 @@ def csv_format(stats:dict, at:Union[datetime.datetime, None]=None, largest_scale
   elif largest_scale == 'm': print('minutes,seconds', end=',')
   elif largest_scale == 's': print('seconds', end=',')
 
-  print('total,id,project,running')
+  print('total,id,project,running', end='')
+  if isinstance(at, datetime.datetime):
+    print(',since')
+  else:
+    print('')
+
   for pid, total in stats.items():
+    if total == 0 and not include_all:
+      continue
+
     if largest_scale == 'w':
       print(f'{weeks_long(total):02}',   end=',')
       print(f'{days_long(total):02}',    end=',')
@@ -232,18 +231,18 @@ def csv_format(stats:dict, at:Union[datetime.datetime, None]=None, largest_scale
       print(f'{seconds_long(total):02}', end=',')
 
     if largest_scale == 'd':
-      print(f'{days_long(total):02}',    end=',')
+      print(f'{days_long(total):03}',    end=',')
       print(f'{hours_long(total):02}',   end=',')
       print(f'{minutes_long(total):02}', end=',')
       print(f'{seconds_long(total):02}', end=',')
 
     if largest_scale == 'h':
-      print(f'{hours_long(total):02}',   end=',')
+      print(f'{hours_long(total):03}',   end=',')
       print(f'{minutes_long(total):02}', end=',')
       print(f'{seconds_long(total):02}', end=',')
 
     if largest_scale == 'm':
-      print(f'{minutes_long(total):02}', end=',')
+      print(f'{minutes_long(total):03}', end=',')
       print(f'{seconds_long(total):02}', end=',')
 
     if largest_scale == 's':
@@ -254,18 +253,26 @@ def csv_format(stats:dict, at:Union[datetime.datetime, None]=None, largest_scale
     print(pid,                         end=',')
     print('"{0}"'.format(db('hget', 'projects', pid)), end=',')
     if isinstance(still_running, UUID) and still_running == UUID(pid):
-      print('true')
+      print('true', end='')
     else:
-      print('false')
+      print('false', end='')
 
-def text_format(stats:dict, at:Union[datetime.datetime, None]=None, largest_scale:str='h', still_running:Union[UUID, None]=None) -> None:
+    if isinstance(at, datetime.datetime):
+      print(f',{at.strftime("%a %F %T")}')
+    else:
+      print('')
+
+def text_format(stats:dict[str, str], at:Union[datetime.datetime, None]=None, largest_scale:str='h', still_running:Union[UUID, None]=None, include_all:bool=False) -> None:
   print('Time spent report', end='')
   if isinstance(at, datetime.datetime):
     print(f' since: {at.strftime("%a %F %T")}')
   else:
-    print()
+    print(f':')
 
   for pid, total in stats.items():
+    if total == 0 and not include_all:
+      continue
+
     if largest_scale == 'w':
       print(f'{weeks_long(total):02}w', end='')
       print(f' {days_long(total):02}d', end='')
@@ -274,18 +281,18 @@ def text_format(stats:dict, at:Union[datetime.datetime, None]=None, largest_scal
       print(f' {seconds_long(total):02}s', end='')
 
     if largest_scale == 'd':
-      print(f'{days_long(total):02}d', end='')
+      print(f'{days_long(total):03}d', end='')
       print(f' {hours_long(total):02}h', end='')
       print(f' {minutes_long(total):02}m', end='')
       print(f' {seconds_long(total):02}s', end='')
 
     if largest_scale == 'h':
-      print(f'{hours_long(total):02}h', end='')
+      print(f'{hours_long(total):03}h', end='')
       print(f' {minutes_long(total):02}m', end='')
       print(f' {seconds_long(total):02}s', end='')
 
     if largest_scale == 'm':
-      print(f'{minutes_long(total):02}m', end='')
+      print(f'{minutes_long(total):03}m', end='')
       print(f' {seconds_long(total):02}s', end='')
 
     if largest_scale == 's':
@@ -300,10 +307,9 @@ def text_format(stats:dict, at:Union[datetime.datetime, None]=None, largest_scal
     else:
       print('')
 
-def report(project:Union[str, UUID, None]=None, at:Union[datetime.datetime, None]=None, largest_scale:str='h', fmt:str='text') -> None:
+def get_stats(project:Union[str, UUID, None]=None, at:Union[datetime.datetime, None]=None) -> dict[str, float]:
   stats = {}
   accum = {}
-  is_running = None
   for tid, log in db('xrange', 'logs', '-', '+'):
     proj = log.get('project')
     actn = log.get('action')
@@ -325,7 +331,6 @@ def report(project:Union[str, UUID, None]=None, at:Union[datetime.datetime, None
 
   last_id, last_name, last_state, last_when = last_project()
   if last_state == 'started':
-    is_running = last_id
     stats[str(last_id)] += int(now().timestamp()-last_when.timestamp())
 
   if project is None:
@@ -333,16 +338,26 @@ def report(project:Union[str, UUID, None]=None, at:Union[datetime.datetime, None
       if isuuid(project_id):
         stats.setdefault(project_id, 0)
 
-  if fmt == 'text':
-    text_format(stats, at, largest_scale, is_running)
-  elif fmt == 'csv':
-    csv_format(stats, at, largest_scale, is_running)
+  return stats
 
-def rename(old:Union[str, UUID], new:str) -> None:
-  id, old_project = load_project(old)
-  db('hset', 'projects', str(id), new)
-  db('hset', 'projects', new.casefold(), str(id))
-  db('save')
+def report(args:argparse.Namespace) -> None:
+  if args.since:
+    when = args.since
+  elif args.between:
+    when = args.between
+  else:
+    when = None
+
+  stats = get_stats(args.project, when)
+  is_running = None
+  last_id, last_name, last_state, last_when = last_project()
+  if last_state == 'started':
+    is_running = last_id
+
+  if args.format == 'text':
+    text_format(stats, when, args.largest_scale, is_running, args.include_all)
+  elif args.format == 'csv':
+    csv_format( stats, when, args.largest_scale, is_running, args.include_all)
 
 def _datetime(dtin:str) -> datetime.datetime:
   weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
@@ -365,6 +380,10 @@ def _datetime(dtin:str) -> datetime.datetime:
       return now() - datetime.timedelta(days=7 - (abbrev_weekdays.index(dtin) - current_dow))
     else:
       return now() - datetime.timedelta(days=current_dow - abbrev_weekdays.index(dtin))
+  elif ':' in dtin and len(hrs_mins := dtin.split(':')) == 2 and all(map(str.isdigit, hrs_mins)) and 0 <= int(hrs_mins[0]) < 24 and 0 <= int(hrs_mins[1]) < 60:
+    return datetime.datetime.strptime(f'{datetime.date.today().strftime("%F")} {hrs_mins[0]}:{hrs_mins[1]}', '%Y-%m-%d %H:%M')
+  elif ':' in dtin and len(hrs_mins_secs := dtin.split(':')) == 3 and all(map(str.isdigit, hrs_mins_secs)) and 0 <= int(hrs_mins[0]) < 24 and 0 <= int(hrs_mins[1]) < 60 and 0 <= int(hrs_mins[2]) < 60:
+    return datetime.datetime.strptime(f'{datetime.date.today().strftime("%F")} {hrs_mins_secs[0]}:{hrs_mins_secs[1]}:{hrs_mins_secs[2]}', '%Y-%m-%d %H:%M:%S')
   else:
     if ' ' in dtin:
       if dtin.casefold().endswith('am') or dtin.casefold().endswith('pm'):
@@ -390,43 +409,43 @@ def pargs() -> argparse.Namespace:
   p = argparse.ArgumentParser(description='Working on Right Now')
   sub = p.add_subparsers(help='Various sub-commands')
   ui = sub.add_parser('gui', help='Show the gui')
-  ui.set_defaults(action='gui', do=gui)
+  ui.set_defaults(action='gui')
 
   beg = sub.add_parser('start', help='Start a project')
-  beg.add_argument('-a', '--at', default=now(), metavar='DATETIME',  help='...the project at this specific time')
+  beg.add_argument('-a', '--at', type=_datetime, default=now(), metavar='DATETIME',  help='...the project at this specific time')
   beg.add_argument('project',    nargs='+',     metavar='NAME|UUID', help='Project name or uuid.')
-  beg.set_defaults(action='start', do=start_project)
+  beg.set_defaults(action='start')
 
   end = sub.add_parser('stop', help='Stop a project')
-  end.add_argument('-a', '--at',                 metavar='DATETIME',  default=now(),  help='...the project at this specific time')
-  end.add_argument('-p', '--project', nargs='+', metavar='NAME|UUID', default='last', help='Project name or uuid (or the last project if none provided).')
-  end.set_defaults(action='stop', do=stop_project)
+  end.add_argument('-a', '--at',      type=_datetime, metavar='DATETIME',  default=now(),  help='...the project at this specific time')
+  end.add_argument('-p', '--project', nargs='+',      metavar='NAME|UUID', default='last', help='Project name or uuid (or the last project if none provided).')
+  end.set_defaults(action='stop')
 
   ren = sub.add_parser('rename', help='Rename a project')
   ren.add_argument('project',    nargs='+', metavar='NAME|UUID',                help='Old project name or uuid.')
   ren.add_argument('-t', '--to', nargs='+', metavar='NAME',      required=True, help='New project name.')
-  ren.set_defaults(action='rename', do=rename)
+  ren.set_defaults(action='rename')
 
   rm = sub.add_parser('rm', help='Remove a project')
   rm.add_argument('project', nargs='+', metavar='NAME|UUID', help='Project name or uuid.')
-  rm.set_defaults(action='rm', do=remove)
+  rm.set_defaults(action='rm')
 
   show = sub.add_parser('show', help='Modify the metadata')
   showsub = show.add_subparsers()
   shl = showsub.add_parser('last', help='Show the last project worked on and what we last did.')
-  shl.set_defaults(action='show_last', do=last)
+  shl.set_defaults(action='show_last')
 
   shp = showsub.add_parser('projects', help='Show the available projects.')
-  shp.set_defaults(action='show_projects', do=show_projects)
+  shp.set_defaults(action='show_projects')
 
   sho = showsub.add_parser('logs',     help='Show the project logs.')
   sho.add_argument('-p', '--project',                       nargs='+', metavar='NAME|UUID', default=None, help='Project name or uuid.')
   sho.add_argument('-s', '--since',         type=_datetime,            metavar='DATETIME',  default=None, help='Report details since this datetime.')
-  sho.set_defaults(action='show_logs', do=show_logs)
+  sho.set_defaults(action='show_logs')
 
   shi = showsub.add_parser('id',       help='Show the project name from the provided id.')
   shi.add_argument('project', metavar='UUID')
-  shi.set_defaults(action='show_id', do=show_id)
+  shi.set_defaults(action='show_id')
 
   rep = sub.add_parser('report', help='Report the results of work done')
   rep.add_argument('-p', '--project',                       nargs='+',                      metavar='NAME|UUID',                               help='Project name or uuid.')
@@ -436,16 +455,17 @@ def pargs() -> argparse.Namespace:
   rep.add_argument('-f', '--format',        type=str,       choices='text,csv'.split(','),                                     default='text', help='Output the report in this format (default: text).')
   rep.add_argument('-u', '--url',           type=urlparse,                                                                                     help='Document the report to this url.')
   rep.add_argument('-m', '--mailto',        type=email,                                                                                        help='Email the report to this user.')
-  rep.set_defaults(action='report', do=report)
+  rep.add_argument('-a', '--include_all',                   action='store_true',                                               default=False,  help='Display ALL projects including those without any tracked time.')
+  rep.set_defaults(action='report')
 
   hlp = sub.add_parser('help', help='Display help')
-  hlp.set_defaults(action='help', do=p.print_help)
+  hlp.set_defaults(action='help')
 
-  p.set_defaults(project=[], action='help', do=p.print_help)
+  p.set_defaults(project=[], action='help')
 
   r = p.parse_args()
   if r.project is not None and len(r.project) > 0 and isinstance(r.project, list):
-    r.project = ' '.join(r.project).strip().replace('\n', '')
+    r.project = ' '.join(r.project).strip().replace('\n', ' ')
 
   if isinstance(r.project, str) and re.search(r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$', r.project):
     r.project = UUID(r.project)
@@ -454,21 +474,32 @@ def pargs() -> argparse.Namespace:
 
 def main() -> None:
   p = pargs()
-  if p.action in ['start', 'stop']:
-    p.do(p.project, p.at)
-  elif p.action in ['rename']:
-    p.do(p.project, p.to)
-  elif p.action in ['rm', 'show_id', 'show_logs']:
-    p.do(p.project)
-  elif p.action in ['gui', 'show_last', 'show_projects', 'help']:
-    p.do()
-  elif p.action in ['report']:
-    if p.since:
-      p.do(p.project, p.since, largest_scale=p.largest_scale, fmt=p.format)
-    elif p.between:
-      p.do(p.project, p.since, largest_scale=p.largest_scale, fmt=p.format)
-    else:
-      p.do(p.project, None, largest_scale=p.largest_scale, fmt=p.format)
+  if p.action == 'show_id':
+    print(db('hget', 'projects', str(id)))
+  elif p.action == 'show_last':
+    print('Last project: {last[1]!r}; action: {last[2]!r}; at: {last[3]}.'.format(last=last_project()))
+  elif p.action == 'show_projects':
+    for pid, project in all_projects().items():
+      print(f'{pid}: {project}')
+  elif p.action == 'show_logs':
+    for tid, log in db('xrange', 'logs', '-', '+'):
+      log_id, log_project = load_project(log.get('project'))
+      if project is None or log_id == project:
+        print(f"{stream_time(tid)} action {log.get('action')!r} project {log_project!r}")
+  elif p.action == 'start':
+    start_project(p.project, p.at)
+  elif p.action == 'stop':
+    stop_project(p.project, p.at)
+  elif p.action == 'rename':
+    rename(p.project, ' '.join(p.to).strip().replace('\n', ' '))
+  elif p.action == 'rm':
+    remove(p.project)
+  elif p.action == 'gui':
+    gui()
+  elif p.action == 'help':
+    p.print_help()
+  elif p.action == 'report':
+    report(p)
 
 if __name__ == '__main__':
   main()
