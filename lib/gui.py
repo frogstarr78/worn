3735896d8bbc204764ca90d24e4e8c93715cb425
Scott Noel-Hemming <frogstarr78@gmail.com>
2024-03-19 10:03:49 -0700

    Simplifying the button handling and adding back the auto-close on button click

--- lib/gui.py
+++ lib/gui.py
@@ -2,21 +2,6 @@ from . import *
 from .project import Project
 from tkinter import ttk, Tk, StringVar, N, S, E, W
 
-def gui_action(event, cb:ttk.Combobox) -> None:
-  button_state = event.widget.cget('text')
-  if 'Start' in button_state:
-    project = Project.make(cb.get())
-    project.start()
-
-    _projects = [project.name for project in Project.all()]
-    cb['values'] = _projects
-    cb['width']  = len(max(_projects, key=len))-10
-  elif button_state == 'Stop':
-    project = Project.make('last')
-    project.stop()
-  else:
-    debug(f"Unknown state {state!r}")
-
 def gui() -> None:
   root = Tk()
   root.title('Worn')
@@ -49,8 +34,6 @@ def gui() -> None:
   hm = ttk.Spinbox(pfrm, from_=0, to=59, width=3, values=list(range(0, 60)), textvariable=mins)
   hm.grid(row=1, column=3, sticky=(W))
 
-
-
 #  s1 = ttk.Style()
 #  s1.configure('Clr.TFrame', background='blue')
 #  bfrm = ttk.Frame(root, padding=11, style='Clr.TFrame')
@@ -71,10 +54,26 @@ def gui() -> None:
   root.bind('<Alt-t>', lambda *e: t.focus_set())
   root.bind('<Alt-q>', lambda *e: root.destroy())
 
-  s.bind('<ButtonPress>',    lambda e: gui_action(e, c))
-  s.bind('<KeyPress-space>', lambda e: gui_action(e, c))
-  t.bind('<ButtonPress>',    lambda e: gui_action(e, c))
-  t.bind('<KeyPress-space>', lambda e: gui_action(e, c))
+  def gui_action(event) -> None:
+    button_state = event.widget.cget('text')
+    if 'Start' in button_state:
+      project = Project.make(c.get())
+      project.start()
+
+      _projects = [project.name for project in Project.all()]
+      c['values'] = _projects
+      c['width']  = len(max(_projects, key=len))-10
+    elif button_state == 'Stop':
+      project = Project.make('last')
+      project.stop()
+    else:
+      debug(f"Unknown state {state!r}")
+    root.destroy()
+
+  s.bind('<ButtonPress>',    lambda e: gui_action(e))
+  s.bind('<KeyPress-space>', lambda e: gui_action(e))
+  t.bind('<ButtonPress>',    lambda e: gui_action(e))
+  t.bind('<KeyPress-space>', lambda e: gui_action(e))
 
   root.columnconfigure(0, weight=5)
   root.rowconfigure(0,    weight=5)
